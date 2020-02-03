a037fe40f64ea087f854d80cef7ff1f37b5d2121
Shikeless <Shikeless@gmail.com>
2020-02-03 23:09:44 +0300

    adding base styles for DraftSection component

--- src/components/DraftSection/DraftSection.js
+++ src/components/DraftSection/DraftSection.js
@@ -1,10 +1,75 @@
 import React from "react";
-import { Box } from "@material-ui/core";
-import { useStyles } from "./DraftSection.styles.js";
+import { Box, TextField, Button, Typography } from "@material-ui/core";
+import {
+  useStyles,
+  DraftButton,
+  SaveButton,
+  RemoveButton
+} from "./DraftSection.styles.js";
 
 function DraftSection() {
   const classes = useStyles();
-  return <Box className={classes.draftSectionBox}>123</Box>;
+  return (
+    <>
+      <Box className={classes.draftSectionBox}>
+        <Box mx={3} my={3}>
+          <TextField
+            id="outlined-size-small"
+            label="Add Draft"
+            variant="outlined"
+            size="small"
+            fullWidth
+          />
+          <Box my={1} display="flex" justifyContent="flex-end">
+            <DraftButton variant="contained">Draft</DraftButton>
+            <SaveButton variant="contained">Save</SaveButton>
+          </Box>
+        </Box>
+      </Box>
+      <Box className={classes.draftSectionBox}>
+        <Box mx={3} my={3}>
+          <Typography variant="h5" gutterBottom>
+            Draft
+          </Typography>
+          <Box mb={4} className={classes.draftItem}>
+            <Typography variant="body1">
+              body1. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Quos blanditiis tenetur unde suscipit, quam beatae rerum inventore
+              consectetur, neque doloribus, cupiditate numquam dignissimos
+              laborum fugiat deleniti? Eum quasi quidem quibusdam.
+            </Typography>
+            <Box display="flex" justifyContent="flex-end">
+              <RemoveButton variant="contained">Remove</RemoveButton>
+              <SaveButton variant="contained" className={classes.saveDraft}>
+                Save
+              </SaveButton>
+            </Box>
+          </Box>
+          <Box mb={4} className={classes.draftItem}>
+            <Typography variant="body1">body1. Lorem ips.</Typography>
+            <Box display="flex" justifyContent="flex-end">
+              <RemoveButton variant="contained">Remove</RemoveButton>
+              <SaveButton variant="contained" className={classes.saveDraft}>
+                Save
+              </SaveButton>
+            </Box>
+          </Box>
+          <Box mb={4} className={classes.draftItem}>
+            <Typography variant="body1">
+              body1. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Quos blanditiis tenetur unde suscipit, quam beatae rerum inventore
+            </Typography>
+            <Box display="flex" justifyContent="flex-end">
+              <RemoveButton variant="contained">Remove</RemoveButton>
+              <SaveButton variant="contained" className={classes.saveDraft}>
+                Save
+              </SaveButton>
+            </Box>
+          </Box>
+        </Box>
+      </Box>
+    </>
+  );
 }
 
 export default DraftSection;
--- src/components/DraftSection/DraftSection.styles.js
+++ src/components/DraftSection/DraftSection.styles.js
@@ -1,7 +1,49 @@
-import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles, withStyles } from "@material-ui/core/styles";
+import { Button } from "@material-ui/core";
+import { grey, lightGreen, red } from "@material-ui/core/colors";
 
 export const useStyles = makeStyles(theme => ({
   draftSectionBox: {
-    backgroundColor: "lightGreen"
+    border: "solid 2px black",
+    marginBottom: "8px"
+  },
+  draftItem: {
+    backgroundColor: "lightgrey",
+    padding: "10px"
+  },
+
+  saveDraft: {
+    position: "relative",
+    top: "20px"
   }
 }));
+
+export const DraftButton = withStyles(theme => ({
+  root: {
+    backgroundColor: grey[400],
+    "&:hover": {
+      backgroundColor: grey[500]
+    }
+  }
+}))(Button);
+
+export const SaveButton = withStyles(theme => ({
+  root: {
+    backgroundColor: lightGreen[400],
+    "&:hover": {
+      backgroundColor: lightGreen[500]
+    },
+    marginLeft: "10px"
+  }
+}))(Button);
+
+export const RemoveButton = withStyles(theme => ({
+  root: {
+    backgroundColor: red[200],
+    "&:hover": {
+      backgroundColor: red[300]
+    },
+    position: "relative",
+    top: "20px"
+  }
+}))(Button);
