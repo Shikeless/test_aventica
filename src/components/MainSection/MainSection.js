801d0fa722ed9742205d054afbca480c89fd2e2a
Shikeless <Shikeless@gmail.com>
2020-02-04 00:33:21 +0300

    adding base styles for MainSection component

--- src/components/MainSection/MainSection.js
+++ src/components/MainSection/MainSection.js
@@ -1,10 +1,44 @@
 import React from "react";
-import { Box } from "@material-ui/core";
-import { useStyles } from "./MainSection.styles.js";
+import { Box, Grid, Typography } from "@material-ui/core";
+import { useStyles, CustomButton } from "./MainSection.styles.js";
 
 function MainSection() {
   const classes = useStyles();
-  return <Box className={classes.mainSectionBox}>123</Box>;
+  return (
+    <Box p={4} className={classes.mainSectionBox}>
+      <Grid container spacing={3}>
+        <Grid item lg={4} md={6} sm={12}>
+          <Box p={1} boxShadow={2} className={classes.activeDraft}>
+            <Box display="flex" justifyContent="flex-end">
+              <Box className={classes.completeIndicator}>&nbsp;</Box>
+            </Box>
+            <Typography variant="body1" gutterBottom>
+              body1. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Quos blanditiis tenetur unde suscipit, quam beatae rerum inventore
+              consectetur, neque doloribus, cupiditate numquam dignissimos
+              laborum fugiat deleniti? Eum quasi quidem quibusdam.
+            </Typography>
+            <Box display="flex" justifyContent="center">
+              <CustomButton variant="contained">Draft</CustomButton>
+              <CustomButton variant="contained">Mark</CustomButton>
+            </Box>
+          </Box>
+        </Grid>
+        <Grid item lg={4} md={6} sm={8}>
+          <Box bgcolor="blue">123</Box>
+        </Grid>
+        <Grid item lg={4} md={6} sm={8}>
+          <Box bgcolor="blue">123</Box>
+        </Grid>
+        <Grid item lg={4} md={6} sm={8}>
+          <Box bgcolor="blue">123</Box>
+        </Grid>
+        <Grid item lg={4} md={6} sm={8}>
+          <Box bgcolor="blue">123</Box>
+        </Grid>
+      </Grid>
+    </Box>
+  );
 }
 
 export default MainSection;
--- src/components/MainSection/MainSection.styles.js
+++ src/components/MainSection/MainSection.styles.js
@@ -1,7 +1,32 @@
-import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles, withStyles } from "@material-ui/core/styles";
+import { Button } from "@material-ui/core";
+import { blueGrey } from "@material-ui/core/colors";
 
 export const useStyles = makeStyles(theme => ({
   mainSectionBox: {
-    backgroundColor: "lightblue"
+    border: "solid 1px grey",
+    backgroundColor: "lightblue",
+    minHeight: "100%"
+  },
+
+  activeDraft: {
+    backgroundColor: "lightgrey",
+    borderRadius: "5px"
+  },
+  completeIndicator: {
+    width: "10px",
+    height: "10px",
+    backgroundColor: "red",
+    borderRadius: "3px"
   }
 }));
+
+export const CustomButton = withStyles(theme => ({
+  root: {
+    backgroundColor: blueGrey[300],
+    "&:hover": {
+      backgroundColor: blueGrey[400]
+    },
+    marginLeft: "10px"
+  }
+}))(Button);
