062c62110f44fd3ff432d3b0b3d0c8590e4bc8ec
Shikeless <Shikeless@gmail.com>
2020-02-04 03:11:18 +0300

    connecting DraftSection component to the store, adding component for draftCard, adding ActionHandlers for input buttons

--- src/components/DraftSection/DraftSection.js
+++ src/components/DraftSection/DraftSection.js
@@ -1,14 +1,65 @@
-import React from "react";
-import { Box, TextField, Button, Typography } from "@material-ui/core";
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import { Box, TextField, Typography } from "@material-ui/core";
 import {
   useStyles,
   DraftButton,
   SaveButton,
   RemoveButton
 } from "./DraftSection.styles.js";
+import { getDraftCards, addToDraft, addToActive } from "../../modules/App";
 
-function DraftSection() {
+const MapStateToProps = state => ({
+  draftCards: getDraftCards(state)
+});
+
+const MapDispatchToProps = { addToDraft, addToActive };
+
+function DraftItem(props) {
   const classes = useStyles();
+
+  return (
+    <Box mb={4} boxShadow={2} className={classes.draftItem}>
+      <Typography variant="body1">{props.item.text}</Typography>
+      <Box display="flex" justifyContent="flex-end">
+        <RemoveButton
+          variant="contained"
+          onClick={() => {
+            console.log(props.item);
+          }}
+        >
+          Remove
+        </RemoveButton>
+        <SaveButton variant="contained" className={classes.saveDraft}>
+          Save
+        </SaveButton>
+      </Box>
+    </Box>
+  );
+}
+
+function DraftSection(props) {
+  const classes = useStyles();
+
+  const [input, setInput] = useState("");
+  const [id, setId] = useState(0);
+
+  const handleChange = event => {
+    setInput(event.target.value);
+  };
+
+  const handleAction = action => {
+    let card;
+    setId(id + 1);
+    if (input === "" || !input.replace(/\s/g, "").length) {
+      card = { id: id, text: "*empty*", marked: false };
+    } else {
+      card = { id: id, text: input, marked: false };
+    }
+    action == "draft" ? props.addToDraft(card) : props.addToActive(card);
+    return setInput("");
+  };
+
   return (
     <>
       <Box mb={1} className={classes.draftSectionBox}>
@@ -17,12 +68,28 @@ function DraftSection() {
             id="outlined-size-small"
             label="Add Draft"
             variant="outlined"
+            value={input}
             size="small"
             fullWidth
+            onChange={handleChange}
           />
           <Box my={1} display="flex" justifyContent="flex-end">
-            <DraftButton variant="contained">Draft</DraftButton>
-            <SaveButton variant="contained">Save</SaveButton>
+            <DraftButton
+              variant="contained"
+              onClick={() => {
+                handleAction("draft");
+              }}
+            >
+              Draft
+            </DraftButton>
+            <SaveButton
+              variant="contained"
+              onClick={() => {
+                handleAction("save");
+              }}
+            >
+              Save
+            </SaveButton>
           </Box>
         </Box>
       </Box>
@@ -31,33 +98,14 @@ function DraftSection() {
           <Typography variant="h5" gutterBottom>
             Draft
           </Typography>
-          <Box mb={4} boxShadow={2} className={classes.draftItem}>
-            <Typography variant="body1">
-              body1. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-              Quos blanditiis tenetur unde suscipit, quam beatae rerum inventore
-              consectetur, neque doloribus, cupiditate numquam dignissimos
-              laborum fugiat deleniti? Eum quasi quidem quibusdam.
-            </Typography>
-            <Box display="flex" justifyContent="flex-end">
-              <RemoveButton variant="contained">Remove</RemoveButton>
-              <SaveButton variant="contained" className={classes.saveDraft}>
-                Save
-              </SaveButton>
-            </Box>
-          </Box>
-          <Box mb={4} boxShadow={2} className={classes.draftItem}>
-            <Typography variant="body1">body1. Lorem ips.</Typography>
-            <Box display="flex" justifyContent="flex-end">
-              <RemoveButton variant="contained">Remove</RemoveButton>
-              <SaveButton variant="contained" className={classes.saveDraft}>
-                Save
-              </SaveButton>
-            </Box>
-          </Box>
+          {props.draftCards &&
+            props.draftCards.map(item => (
+              <DraftItem key={item.id} item={item} />
+            ))}
         </Box>
       </Box>
     </>
   );
 }
 
-export default DraftSection;
+export default connect(MapStateToProps, MapDispatchToProps)(DraftSection);
